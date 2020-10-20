18a0c85cc10b3b9a5c22d02abd2fd0dbb8cfef4e
azusa5526 <azusa5526@gmail.com>
2020-10-20 15:16:04 +0800

    before fix swiper

--- js/swiperInit.js
+++ js/swiperInit.js
@@ -1,13 +1,22 @@
-let mySwiper = new Swiper(".swiper-container", {
-  direction: "horizontal",
-  loop: true,
-
-  pagination: {
-    el: ".swiper-pagination",
-  },
+let swiper1 = new Swiper(".swiper1", {
+  pagination: ".swiper-pagination1",
+  paginationClickable: true,
 
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+});
+
+let swiper2 = new Swiper(".swiper-container", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  autoplay: { delay: 5000 },
+
+  pagination: {
+    el: ".swiper-pagination2",
+    clickable: true,
   },
 });
