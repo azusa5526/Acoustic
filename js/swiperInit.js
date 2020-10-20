303d59f5bb16e90369d9f5472e06aeaf4d9941f1
azusa5526 <azusa5526@gmail.com>
2020-10-21 01:57:41 +0800

    products basic rwd

--- js/swiperInit.js
+++ js/swiperInit.js
@@ -1,22 +1,38 @@
+// let swiper1 = new Swiper(".swiper1", {
+//   pagination: ".swiper-pagination1",
+//   paginationClickable: true,
+
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+// let swiper2 = new Swiper(".swiper-container", {
+//   slidesPerView: "auto",
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   loop: true,
+//   autoplay: { delay: 5000 },
+
+//   pagination: {
+//     el: ".swiper-pagination2",
+//     clickable: true,
+//   },
+// });
+
 let swiper1 = new Swiper(".swiper1", {
+  loop: true,
+  autoplay: { delay: 4000 },
   pagination: ".swiper-pagination1",
   paginationClickable: true,
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
 });
-
-let swiper2 = new Swiper(".swiper-container", {
-  slidesPerView: "auto",
+let swiper2 = new Swiper(".swiper2", {
+  slidesPerView: "3",
   spaceBetween: 30,
   centeredSlides: true,
   loop: true,
-  autoplay: { delay: 5000 },
-
-  pagination: {
-    el: ".swiper-pagination2",
-    clickable: true,
-  },
+  autoplay: { delay: 4000 },
+  pagination: ".swiper-pagination2",
+  paginationClickable: true,
 });
