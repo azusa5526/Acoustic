9033e84c0c063bb8f2b58455d3f787ee92c61a49
azusa5526 <azusa5526@gmail.com>
2020-10-21 22:00:38 +0800

    news done

--- js/swiperInit.js
+++ js/swiperInit.js
@@ -28,11 +28,25 @@ let swiper1 = new Swiper(".swiper1", {
   paginationClickable: true,
 });
 let swiper2 = new Swiper(".swiper2", {
-  slidesPerView: "3",
-  spaceBetween: 30,
+  slidesPerView: "1",
+  spaceBetween: 10,
   centeredSlides: true,
   loop: true,
-  autoplay: { delay: 4000 },
+  autoplay: { delay: 3000 },
   pagination: ".swiper-pagination2",
   paginationClickable: true,
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    1200: {
+      slidesPerView: 3.5,
+      spaceBetween: 25,
+    },
+  }
 });
